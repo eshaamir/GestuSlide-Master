73267bc7f18731a59a1141e80aa0726196a44cc8
ESHA AMIR <75741369+eshaamir@users.noreply.github.com>
2024-07-05 15:55:07 +0500

    images.js was added

---
+++ constants/images.js
@@ -0,0 +1,39 @@
+import header_main_img from "../assets/images/header_main_img.jpg";
+import about_main_img from "../assets/images/about_main_img.jpg";
+import bgheader from "../assets/images/bgheader.jpeg";
+import form_main_img from "../assets/images/form_main_img.jpg";
+import hands from "../assets/images/hands.png";
+import professionals_main_img from "../assets/images/professionals_main_img.jpg";
+import video_cover_img from "../assets/images/video_cover_img.jpg";
+import customer_img_1 from "../assets/images/customer_img_1.jpg";
+import customer_img_2 from "../assets/images/customer_img_2.jpg";
+import customer_img_3 from "../assets/images/customer_img_3.jpg";
+import customer_img_4 from "../assets/images/customer_img_4.jpg";
+import customer_img_5 from "../assets/images/customer_img_5.jpg";
+import customer_img_6 from "../assets/images/customer_img_6.jpg";
+import contactus from "../assets/images/contactus.jpg";
+import portfolio_img_1 from "../assets/images/portfolio_img_1.jpg";
+import portfolio_img_2 from "../assets/images/portfolio_img_2.jpg";
+import portfolio_img_3 from "../assets/images/portfolio_img_3.jpg";
+import gestures from "../assets/images/gestures.jpg";
+import vector from "../assets/images/vector.png";
+import ugls from "../assets/images/ugls.jpg";
+import CUSVIDEO from  "../assets/images/CUSVIDEO.mp4";
+import CUGIF from  "../assets/images/CUGIF.gif";
+import Phone from  "../assets/images/Phone.png";
+import CGC from "../assets/images/CGC.png";
+import FE from "../assets/images/FE.png";
+import HC from "../assets/images/HC.png";
+import LP from "../assets/images/LP.png";
+import PS from "../assets/images/PS.png";
+import SC from "../assets/images/SC.png";
+import bg from "../assets/images/bg.webp";
+import login from "../assets/images/login.png";
+import camera from "../assets/images/camera.png";
+import blog from "../assets/images/blog.webp";
+import about from "../assets/images/about.avif";
+import explore from "../assets/images/explore.jpg"
+
+const images = {header_main_img, about_main_img, form_main_img,ugls, login,about,explore,contactus,camera,blog,bg,hands,vector,bgheader,CUSVIDEO,CUGIF,Phone,professionals_main_img, video_cover_img, customer_img_1, customer_img_2, customer_img_3, customer_img_4, customer_img_5, customer_img_6, portfolio_img_1, portfolio_img_2, portfolio_img_3, gestures, CGC, FE, HC, LP, PS, SC, explore};
+
+export default images;
